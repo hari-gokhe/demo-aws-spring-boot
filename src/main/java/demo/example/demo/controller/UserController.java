c70196a6599a2766f7ae695bdfcd02c74e7b06e0
hari-gokhe <harigokhe19@gmail.com>
2023-10-21 14:28:34 +0530

    adding fist commit

---
+++ src/main/java/demo/example/demo/controller/UserController.java
@@ -0,0 +1,14 @@
+package demo.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("welcome")
+    public String index() {
+        return "welcome to spring boot project";
+    }
+}
